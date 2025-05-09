78664d66b79323b005445214ec0475a0cc39d9ac
Maksym Kozak <bersercer70@gmail.com>
2025-05-10 00:53:22 +0300

    create user entity and configure connection to development database

---
+++ auth/src/main/java/com/fastify/auth/entity/User.java
@@ -0,0 +1,30 @@
+package com.fastify.auth.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "fastify_user")
+@NoArgsConstructor
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+
+
+}
