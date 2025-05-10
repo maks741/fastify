725ce8a280bb901127a753d392b9a46023ad0c43
Maksym Kozak <bersercer70@gmail.com>
2025-05-10 12:19:58 +0300

    start with JwtAuthenticationFilter and add JwtService

---
+++ auth/src/main/java/com/fastify/auth/security/JwtAuthenticationFilter.java
@@ -0,0 +1,38 @@
+package com.fastify.auth.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
+        String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+        String bearerPrefix = "Bearer ";
+        String jwt;
+
+        if (authorizationHeader == null || !authorizationHeader.startsWith(bearerPrefix)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        jwt = authorizationHeader.substring(bearerPrefix.length());
+    }
+}
---
+++ auth/src/main/java/com/fastify/auth/security/JwtService.java
@@ -0,0 +1,82 @@
+package com.fastify.auth.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+import javax.crypto.SecretKey;
+import java.util.Collections;
+import java.util.Date;
+import java.util.Map;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+
+    private final String jwtSigningKey;
+    private final Long jwtExpirationPeriod;
+
+    public JwtService(
+            @Value("${jwt.signing.key}") String jwtSigningKey,
+            @Value("${jwt.expiration.period}") Long jwtExpirationPeriod
+    ) {
+        this.jwtSigningKey = jwtSigningKey;
+        this.jwtExpirationPeriod = jwtExpirationPeriod;
+    }
+
+    public String extractUsername(String jwt) {
+        return extractClaims(jwt, Claims::getSubject);
+    }
+
+    public boolean validate(String jwt, UserDetails userDetails) {
+        String username = extractUsername(jwt);
+        return username.equals(userDetails.getUsername()) && tokenNotExpired(jwt);
+    }
+
+    public String generateToken(UserDetails userDetails) {
+        return generateToken(Collections.emptyMap(), userDetails);
+    }
+
+    public String generateToken(
+            Map<String, Object> claims,
+            UserDetails userDetails
+    ) {
+        long currentTimeMillis = System.currentTimeMillis();
+        Date issuedAt = new Date(currentTimeMillis);
+        Date expiration = new Date(currentTimeMillis + jwtExpirationPeriod);
+
+        return Jwts.builder()
+                .claims(claims)
+                .subject(userDetails.getUsername())
+                .issuedAt(issuedAt)
+                .expiration(expiration)
+                .signWith(signingKey())
+                .compact();
+    }
+
+    private boolean tokenNotExpired(String jwt) {
+        Date expirationDate = extractClaims(jwt, Claims::getExpiration);
+        Date now = new Date(System.currentTimeMillis());
+        return expirationDate.after(now);
+    }
+
+    private <T> T extractClaims(String jwt, Function<Claims, T> claimsResolver) {
+        Claims claims = extractClaims(jwt);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractClaims(String jwt) {
+        return Jwts.parser()
+                .verifyWith(signingKey())
+                .build()
+                .parseSignedClaims(jwt)
+                .getPayload();
+    }
+
+    private SecretKey signingKey() {
+        byte[] keyBytes = jwtSigningKey.getBytes();
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
