fe5e445f5f3ea6fd4f724a56c5c7b98e94c70c25
Maksym Kozak <bersercer70@gmail.com>
2025-05-10 11:18:45 +0300

    use UserService in the controller

--- auth/src/main/java/com/fastify/auth/controller/AuthController.java
+++ auth/src/main/java/com/fastify/auth/controller/AuthController.java
@@ -2,6 +2,9 @@ package com.fastify.auth.controller;
 
 import com.fastify.auth.model.dto.SignUpRequestDto;
 import com.fastify.auth.model.dto.SignUpResponseDto;
+import com.fastify.auth.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,11 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
+@RequiredArgsConstructor
 public class AuthController {
+
+    private final UserService userService;
 
     @PostMapping("/signup")
     public ResponseEntity<SignUpResponseDto> signUp(@RequestBody SignUpRequestDto signUpRequestDto) {
-
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.signUp(signUpRequestDto));
     }
 
 
