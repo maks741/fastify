e959bde83114621fa254c57e148c461f506998c6
Maksym Kozak <bersercer70@gmail.com>
2025-05-17 00:32:37 +0300

    add initial listen microservice

---
+++ listen/src/test/java/com/fastify/listen/ListenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastify.listen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
