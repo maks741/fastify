63a3b1061ad503216884c05ab4d13a5c98df01d8
Maksym Kozak <bersercer70@gmail.com>
2025-05-16 20:03:42 +0300

    basic setup for kafka in auth service

---
+++ auth/src/main/java/com/fastify/auth/config/KafkaTopicConfig.java
@@ -0,0 +1,31 @@
+package com.fastify.auth.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+@ConfigurationProperties(prefix = "spring.kafka")
+public class KafkaTopicConfig {
+
+    // I assume it will go and read spring.kafka.topics property
+    // and map the values of that property to that list
+    private List<TopicConfig> topics;
+
+    @Bean
+    public List<NewTopic> createTopics() {
+        return topics.stream()
+                .map(topic -> new NewTopic(topic.name, topic.partitions, topic.replicationFactor))
+                .toList();
+    }
+
+    record TopicConfig(
+            String name,
+            int partitions,
+            short replicationFactor
+    ) {
+    }
+}
