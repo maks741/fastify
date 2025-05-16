4354162f90d9cb90b8694bcf402aa2d60d63e5a3
Maksym Kozak <bersercer70@gmail.com>
2025-05-16 20:12:44 +0300

    setup fromString method that will allow us to convert string command type from kafka message header into UserCommand

--- music-download/src/main/java/com/fastify/musicdownload/model/command/UserCommand.java
+++ music-download/src/main/java/com/fastify/musicdownload/model/command/UserCommand.java
@@ -1,5 +1,18 @@
 package com.fastify.musicdownload.model.command;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public enum UserCommand {
-    CREATE
+    CREATE,
+    UNKNOWN;
+
+    public static UserCommand fromString(String s) {
+        try {
+            return valueOf(s);
+        } catch (IllegalArgumentException e) {
+            log.warn("Received unknown command: {}", e.getMessage());
+            return UNKNOWN;
+        }
+    }
 }
