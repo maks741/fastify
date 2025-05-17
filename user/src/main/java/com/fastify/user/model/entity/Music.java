0d323364483a3e4d77caf11187146652325340e6
Maksym Kozak <bersercer70@gmail.com>
2025-05-17 16:06:37 +0300

    fix errors with saving Music instance to repo

--- user/src/main/java/com/fastify/user/model/entity/Music.java
+++ user/src/main/java/com/fastify/user/model/entity/Music.java
@@ -3,6 +3,7 @@ package com.fastify.user.model.entity;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -12,12 +13,14 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
 @Table(name = "music")
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@ToString(exclude = "user")
 @Builder
 public class Music {
 
@@ -30,6 +33,6 @@ public class Music {
     @Column(nullable = false, columnDefinition = "TEXT")
     private String url;
 
-    @ManyToOne(cascade = CascadeType.ALL)
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private User user;
 }
--- user/src/main/java/com/fastify/user/model/entity/User.java
+++ user/src/main/java/com/fastify/user/model/entity/User.java
@@ -1,6 +1,7 @@
 package com.fastify.user.model.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
@@ -8,6 +9,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,7 +25,7 @@ public class User {
     @Id
     private Long id;
 
-    @OneToMany(mappedBy = "user")
+    @OneToMany(mappedBy = "user", fetch = FetchType.EAGER)
     private List<Music> playlist;
 
     public User(Long id) {
