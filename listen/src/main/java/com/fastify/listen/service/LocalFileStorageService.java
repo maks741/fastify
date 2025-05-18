11b21f8424ab55d8d19380ce47870ca1685fe88d
Maksym Kozak <bersercer70@gmail.com>
2025-05-18 17:25:37 +0300

    remove hardcoded strings from LocalFileStorageService in listen microservice

--- listen/src/main/java/com/fastify/listen/service/LocalFileStorageService.java
+++ listen/src/main/java/com/fastify/listen/service/LocalFileStorageService.java
@@ -11,17 +11,23 @@ import org.springframework.stereotype.Service;
 public class LocalFileStorageService implements FileStorage {
 
     private final String localStorageHost;
+    private final String audioSuffix;
+    private final String audioFormat;
 
     public LocalFileStorageService(
-            @Value("${local.storage.host}") String localStorageHost
+            @Value("${local.storage.host}") String localStorageHost,
+            @Value("${local.objects.suffix.audio}") String audioSuffix,
+            @Value("${local.objects.format.audio}") String audioFormat
     ) {
         this.localStorageHost = localStorageHost;
+        this.audioSuffix = audioSuffix;
+        this.audioFormat = audioFormat;
     }
 
     @Override
     public ListenResponse generateAudioUrl(UserClaims userClaims, String videoId) {
         String storageSubFolderPath = userClaims.userId() + "/" + videoId + "/";
-        String thumbnailPathStr = storageSubFolderPath + "audio" + "." + "mp3";
+        String thumbnailPathStr = storageSubFolderPath + audioSuffix + "." + audioFormat;
 
         String audioResourceUrl = localStorageHost + thumbnailPathStr;
         return new ListenResponse(videoId, audioResourceUrl);
