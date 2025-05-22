114f810842ee2fad152f57a0b6d92f7ab189feb5
Maksym Kozak <bersercer70@gmail.com>
2025-05-22 23:06:42 +0300

    more tests for signup method

--- auth/src/test/java/com/fastify/auth/service/UserServiceImplTest.java
+++ auth/src/test/java/com/fastify/auth/service/UserServiceImplTest.java
@@ -1,7 +1,9 @@
 package com.fastify.auth.service;
 
 import com.fastify.auth.controller.kafka.UserCommandProducer;
+import com.fastify.auth.exception.DuplicateEmailException;
 import com.fastify.auth.exception.NotFoundException;
+import com.fastify.auth.exception.PublishingException;
 import com.fastify.auth.model.command.UserCommand;
 import com.fastify.auth.model.dto.LoginRequestDto;
 import com.fastify.auth.model.dto.LoginResponseDto;
@@ -18,6 +20,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.AuthenticationException;
@@ -27,6 +30,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -118,6 +122,90 @@ class UserServiceImplTest {
                 .sendUserCommand(userCommandTopic, savedUserId, userCreatedEventJson, UserCommand.CREATE);
         verify(jwtService).generateToken(expectedSavedUser);
         assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void signUp_WhenUserCommandProducerThrowsPublishingException_ShouldThrowPublishingException() {
+        Long savedUserId = 2L;
+        String username = "username";
+        String email = "email@email.com";
+        String password = "password";
+        String encodedPassword = "encoded password";
+        Role userRole = Role.USER;
+        SignUpRequestDto signUpRequestDto = new SignUpRequestDto(username, email, password);
+        User expectedSavedUser = User.builder()
+                .id(savedUserId)
+                .name(username)
+                .email(email)
+                .password(encodedPassword)
+                .role(userRole)
+                .build();
+        User expectedUserToSave = User.builder()
+                .name(username)
+                .email(email)
+                .password(encodedPassword)
+                .role(userRole)
+                .build();
+        UserCreatedEvent userCreatedEvent = new UserCreatedEvent(
+                savedUserId,
+                email,
+                userRole
+        );
+        String userCreatedEventJson = "json";
+
+        when(passwordEncoder.encode(password))
+                .thenReturn(encodedPassword);
+        when(userRepository.save(expectedUserToSave))
+                .thenReturn(expectedSavedUser);
+        when(jsonConverter.toJson(userCreatedEvent))
+                .thenReturn(userCreatedEventJson);
+        doThrow(new PublishingException())
+                .when(userCommandProducer).sendUserCommand(userCommandTopic, savedUserId, userCreatedEventJson, UserCommand.CREATE);
+
+        assertThrows(
+                PublishingException.class,
+                () -> userService.signUp(signUpRequestDto)
+        );
+
+        verify(passwordEncoder).encode(password);
+        verify(userRepository).save(expectedUserToSave);
+        verify(jsonConverter).toJson(userCreatedEvent);
+        verify(userCommandProducer)
+                .sendUserCommand(userCommandTopic, savedUserId, userCreatedEventJson, UserCommand.CREATE);
+        verify(jwtService, never()).generateToken(expectedSavedUser);
+    }
+
+    @Test
+    void signUp_WhenUserWithGivenEmailAlreadyExists_ShouldThrowDuplicateEmailException() {
+        String username = "username";
+        String email = "email@email.com";
+        String password = "password";
+        String encodedPassword = "encoded password";
+        Role userRole = Role.USER;
+        SignUpRequestDto signUpRequestDto = new SignUpRequestDto(username, email, password);
+        User expectedUserToSave = User.builder()
+                .name(username)
+                .email(email)
+                .password(encodedPassword)
+                .role(userRole)
+                .build();
+
+        when(passwordEncoder.encode(password))
+                .thenReturn(encodedPassword);
+        when(userRepository.save(expectedUserToSave))
+                .thenThrow(new DataIntegrityViolationException(""));
+
+        assertThrows(
+                DuplicateEmailException.class,
+                () -> userService.signUp(signUpRequestDto)
+        );
+
+        verify(passwordEncoder).encode(password);
+        verify(userRepository).save(expectedUserToSave);
+        verify(jsonConverter, never()).toJson(any());
+        verify(userCommandProducer, never())
+                .sendUserCommand(any(), any(), any(), any());
+        verify(jwtService, never()).generateToken(any());
     }
 
     @Test
